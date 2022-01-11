b0d5030f84a87a47a94607a069ef1cbbc9201bd9
ilfan <ilfanmuratov@gmail.com>
2022-01-11 20:59:26 +0500

    logo in admin

--- config/settings.py
+++ config/settings.py
@@ -134,7 +134,7 @@ USE_TZ = True
 STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 STATIC_DIR = os.path.join(BASE_DIR, 'static')
-STATICFILES_DIRS = ['static/frontend/dist/static/']
+STATICFILES_DIRS = ['static/frontend/dist/static/', 'static/logo/']
 
 # Media files
 MEDIA_URL = '/media/'
