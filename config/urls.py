02d2e5bd7a2bb10b13ca681e67e6e41e49155a50
ilfan <ilfanmuratov@gmail.com>
2022-01-11 17:50:36 +0500

    urls to start

--- config/urls.py
+++ config/urls.py
@@ -1,7 +1,7 @@
 from django.conf import settings
 from django.conf.urls.static import static
 from django.contrib import admin
-from django.urls import path, include
+from django.urls import path, include, re_path
 
 urlpatterns = [
     path('api/v1/', include([
@@ -17,12 +17,8 @@ urlpatterns = [
     # * Документация
     path('', include('spectacular.urls')),
     path('ckeditor/', include('ckeditor_uploader.urls')),
-]
-
-if settings.DEBUG:
-    urlpatterns += static(settings.MEDIA_URL,
-                          document_root=settings.MEDIA_ROOT)
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 
 urlpatterns += [
-
+    re_path(r'^.*', include('start.urls'))
 ]
